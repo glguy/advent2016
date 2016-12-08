33b97150700a4229977fcf2262be2f2b1b2f5e31
Eric Mertens <emertens@galois.com>
2016-12-07 23:32:04 -0800

    Do in-place array rotations

--- Day08.hs
+++ Day08.hs
@@ -1,15 +1,23 @@
-module Main where
+{-# Language TupleSections #-}
+module Main (main) where
 
 import Data.Array.IO
+import Data.Char
 import Data.Foldable
+import Data.Function
+import Data.List
 import Control.Concurrent
 import Control.Monad
+
+rows, cols :: Int
+rows = 6
+cols = 50
 
 data Command
   = Rect      !Int !Int
   | RotateCol !Int !Int
   | RotateRow !Int !Int
-  deriving (Read, Show, Eq, Ord)
+  deriving Show
 
 main :: IO ()
 main =
@@ -18,14 +26,14 @@ main =
 
 interp :: [Command] -> IO ()
 interp cmds =
-  do a <- newArray ((0,0),(49,5)) False
+  do a <- newArray ((0,0),(cols-1,rows-1)) False
           :: IO (IOUArray (Int,Int) Bool)
 
      for_ cmds $ \cmd ->
        do interpCommand a cmd
           print cmd
           drawScreen a
-          threadDelay 75000
+          threadDelay 25000
 
      n <- countPixels a
      putStrLn ("Pixels: " ++ show n)
@@ -33,33 +41,13 @@ interp cmds =
 drawScreen :: IOUArray (Int,Int) Bool -> IO ()
 drawScreen a =
   for_ [0..5] $ \y ->
-    do xs <- readRow a y
+    do xs <- traverse (\x -> readArray a (x,y)) [0..cols-1]
        putStrLn (map toBlock xs)
 
 countPixels :: IOUArray (Int,Int) Bool -> IO Int
 countPixels a =
   do xs <- getElems a
      return $! length (filter id xs)
-
-readRow :: MArray a e m => a (Int,Int) e -> Int -> m [e]
-readRow a y =
-  do ((xlo,_),(xhi,_)) <- getBounds a
-     traverse (\x -> readArray a (x,y)) [xlo..xhi]
-
-readCol :: MArray a e m => a (Int,Int) e -> Int -> m [e]
-readCol a x =
-  do ((_,ylo),(_,yhi)) <- getBounds a
-     traverse (\y -> readArray a (x,y)) [ylo..yhi]
-
-writeRow :: MArray a e m => a (Int,Int) e -> Int -> [e] -> m ()
-writeRow a y xs =
-  do ((xlo,_),(xhi,_)) <- getBounds a
-     zipWithM_ (\x -> writeArray a (x,y)) [xlo..xhi] xs
-
-writeCol :: MArray a e m => a (Int,Int) e -> Int -> [e] -> m ()
-writeCol a x ys =
-  do ((_,ylo),(_,yhi)) <- getBounds a
-     zipWithM_ (\y -> writeArray a (x,y)) [ylo..yhi] ys
 
 toBlock :: Bool -> Char
 toBlock True  = '█'
@@ -71,31 +59,31 @@ interpCommand a (Rect x y) =
   for_ [0 .. y-1] $ \y ->
   writeArray a (x,y) True
 
-interpCommand a (RotateCol x n) =
-  do xs <- readCol a x
-     writeCol a x (rotate n xs)
+interpCommand a (RotateCol x n) = rotate a (x,) rows n
+interpCommand a (RotateRow y n) = rotate a (,y) cols n
 
-interpCommand a (RotateRow y n) =
-  do xs <- readRow a y
-     writeRow a y (rotate n xs)
+rotate :: (Ix i, MArray a e m) => a i e -> (Int -> i) -> Int -> Int -> m ()
+rotate a f len n =
+  do reverseRange a f 0 (len-1-n)
+     reverseRange a f (len-n) (len-1)
+     reverseRange a f 0 (len-1)
 
-rotate :: Int -> [a] -> [a]
-rotate n xs = b ++ a
-  where
-    len   = length xs
-    (a,b) = splitAt ((-n) `mod` len) xs
+reverseRange :: (Ix i, MArray a e m) => a i e -> (Int -> i) -> Int -> Int -> m ()
+reverseRange a f lo hi =
+  when (lo < hi) $
+    do swap a (f lo) (f hi)
+       reverseRange a f (lo+1) (hi-1)
 
-lexer :: String -> [String]
-lexer "" = []
-lexer xs =
-  case lex xs of
-    [(x,ys)] -> x : lexer ys
-    _ -> error ("lexer: " ++ xs)
+swap :: (MArray a e m, Ix i) => a i e -> i -> i -> m ()
+swap a i j =
+  do t <- readArray a i
+     writeArray a i =<< readArray a j
+     writeArray a j t
 
 parseCommand :: String -> Command
 parseCommand cmd =
-  case lexer cmd of
-    ["rotate","row","y","=",y,"by",n]    -> RotateRow (read y) (read n)
-    ["rotate","column","x","=",x,"by",n] -> RotateCol (read x) (read n)
-    ["rect",x,'x':y]                     -> Rect (read x) (read y)
-    _ -> error cmd
+  case groupBy ((==) `on` isDigit) cmd of
+    ["rotate row y="   ,y," by ",n] -> RotateRow (read y) (read n)
+    ["rotate column x=",x," by ",n] -> RotateCol (read x) (read n)
+    ["rect "           ,x,"x"   ,y] -> Rect      (read x) (read y)
+    _                               -> error cmd
