e63e0bded2252861e07680fd889f3244702cfb86
Eric Mertens <emertens@gmail.com>
2016-12-13 21:45:15 -0800

    Add day 14

---
+++ Day14.hs
@@ -0,0 +1,51 @@
+{-# Language OverloadedStrings #-}
+module Main where
+
+import Control.Monad
+import Crypto.Hash
+import Data.ByteArray (convert)
+import Data.ByteString.Builder
+import Data.ByteString.Builder.Extra
+import Data.List
+import Data.Monoid
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy as L
+
+myinp :: B.ByteString
+myinp = "qzyelonm"
+
+main :: IO ()
+main =
+  do print (solve 1)
+     print (solve 2017)
+
+-- | Hash a bytestring to to ASCII encoded, lowercase hex
+hashmd5 :: B.ByteString -> B.ByteString
+hashmd5 str
+  = L.toStrict
+  $ toLazyByteStringWith md5strategy L.empty
+  $ byteStringHex
+  $ convert (hash str :: Digest MD5)
+  where
+    md5strategy = untrimmedStrategy 32 32
+
+iteratedHash :: Int -> B.ByteString -> B.ByteString
+iteratedHash n x
+  | n <= 0 = x
+  | otherwise = iteratedHash (n-1) (hashmd5 x)
+
+seed :: Int -> B.ByteString
+seed i = myinp <> B8.pack (show i)
+
+solve :: Int -> Int
+solve iterations =
+  search 0 (map (B8.unpack . iteratedHash iterations . seed) [0..]) !! 63
+
+search :: Int -> [String] -> [Int]
+search _ [] = []
+search i (h:hs) =
+  do start <- take 1 [ x | x:y:z:_ <- tails h, x==y, y==z]
+     guard (any (replicate 5 start `isInfixOf`) (take 1000 hs))
+     return i
+  ++ search (i+1) hs
