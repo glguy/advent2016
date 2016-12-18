fca661289600d9cd32832a1490e20f0aa56698a4
Eric Mertens <emertens@galois.com>
2016-12-18 01:04:05 -0600

    Update cabal file, tweak day 18

--- Day18.hs
+++ Day18.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Common
+import Common (count, readInputFile)
 import Data.List (tails)
 
 rule :: Char -> Char -> Char
@@ -9,13 +9,13 @@ rule x y
   | otherwise = '^'
 
 next :: String -> String
-next xs = [ rule x y | x:_:y:_ <- tails ('.':xs++['.'])]
+next xs = [ rule x y | x:_:y:_ <- tails ("." ++ xs ++ ".") ]
 
 problem :: String -> Int -> Int
 problem input n =
-  count (=='.') $ concat $ take n $ iterate next input
+  count ('.'==) $ concat $ take n $ iterate next input
 
 main =
-  do input <- filter (`elem` "^.") <$> readInputFile 18
+  do input <- head . lines <$> readInputFile 18
      print (problem input     40)
      print (problem input 400000)
