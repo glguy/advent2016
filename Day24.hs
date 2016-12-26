2efd1f98ff7ee1f7b37b3e367e82643ec3616681
Eric Mertens <emertens@galois.com>
2016-12-26 11:47:46 -0600

    Speed up Day24 with a faster "seen" set type

--- Day24.hs
+++ Day24.hs
@@ -2,13 +2,19 @@
 module Main where
 
 import           Common
+import           Data.Bits
 import           Data.Char
-import           Data.Set ( Set )
-import qualified Data.Set as Set
+import           Data.Foldable
+import           Data.Maybe
 import           GridCoord
 import           Search
 import qualified Data.Array.Unboxed as Array
 import           Data.Array.Unboxed (Array)
+
+type DigitSet = Int
+
+data Entry = Entry {-# UNPACK #-} !Coord !DigitSet
+  deriving (Eq, Ord)
 
 parseInput :: String -> Array Coord Char
 parseInput file =
@@ -22,31 +28,41 @@ main :: IO ()
 main =
   do maze <- parseInput <$> readInputFile 24
 
-     let targets = count isDigit (Array.elems maze)
+     let targets =
+           foldl' setBit 0
+                  $ mapMaybe digitToInt'
+                  $ Array.elems maze
+
          [start] = [ c | (c,x) <- Array.assocs maze, x == '0' ]
 
          endings =
            [ (here,steps)
               | (seen,here,steps) <-
                     bfsOn
-                      (\(seen,here,_steps) -> (here,seen))
+                      (\(seen,here,_steps) -> Entry here seen)
                       (next maze)
-                      (Set.singleton '0',start,0)
-              , Set.size seen == targets ]
+                      (bit 0, start,0)
+              , seen == targets ]
 
      print $ head [ steps | (_  ,steps) <- endings ]
      print $ head [ steps | (end,steps) <- endings, end == start ]
 
 next ::
   Array Coord Char         ->
-  (Set Char, Coord, Int) ->
-  [(Set Char, Coord, Int)]
+  (DigitSet, Coord, Int) ->
+  [(DigitSet, Coord, Int)]
 next maze (seen,here,steps) =
   [ (seen',here',steps')
     | let !steps' = steps + 1
     , here' <- cardinalNeighbors here
     , let x = maze Array.! here'
     , x /= '#'
-    , let !seen' | isDigit x = Set.insert x seen
-                 | otherwise = seen
+    , let !seen' = case digitToInt' x of
+                     Just i -> setBit seen i
+                     Nothing -> seen
     ]
+
+digitToInt' :: Char -> Maybe Int
+digitToInt' x
+  | isDigit x = Just (digitToInt x)
+  | otherwise = Nothing
