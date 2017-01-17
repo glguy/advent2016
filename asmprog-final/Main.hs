53e969bb8090d9b5a06858bea08ae833140be657
Eric Mertens <emertens@galois.com>
2017-01-16 20:04:14 -0800

    Improvements to asmprog-final

--- asmprog-final/Main.hs
+++ asmprog-final/Main.hs
@@ -2,50 +2,47 @@
 module Main (main, runUntilEnd) where
 
 import           Control.Applicative
-import           Control.Exception
+import           Control.Exception (throwIO)
 import           Control.Monad
 import           Control.Monad.Primitive
 import           Control.Monad.Trans.State
 import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Writer
-import           Data.Functor.Identity
 import           Data.Foldable
 import           Data.Maybe
 import           Data.Monoid
+import qualified Data.Text.IO as Text
 import qualified Data.Vector as Vector
 import           Data.Vector (Vector)
 import qualified Data.Vector.Unboxed.Mutable as MV
 import           Text.Megaparsec
-import           Text.Megaparsec.String
+import           Text.Megaparsec.Text
 import qualified Text.Megaparsec.Lexer as L
-import           Debug.Trace
 
 filename12, filename23, filename25 :: FilePath
-filename12 = "/Users/emertens/Source/advent2016/inputs/input12.txt"
-filename23 = "/Users/emertens/Source/advent2016/inputs/input23.txt"
-filename25 = "/Users/emertens/Source/advent2016/inputs/input25.txt"
+filename12 = "../inputs/input12.txt"
+filename23 = "../inputs/input23.txt"
+filename25 = "../inputs/input25.txt"
 
 main :: IO ()
 main =
-  do program12 <- loadFile basicInstructions filename12
-     let compute12 c = fst $ runIdentity $ runMachineT initialRegisters
-                     $ C =: c >> runUntilEnd program12 >> reg A
-     putStrLn "Solutions for problem 12"
-     print (compute12 0)
-     print (compute12 1)
+  do putStrLn "Problem 12"
+     program12 <- loadFile basicInstructions filename12
+     let compute12 c = evalMachineT
+                     $ do C =: c; runUntilEnd program12; reg A
+     print =<< compute12 0
+     print =<< compute12 1
 
+     putStrLn "Problem 23"
      program23 <- loadFile (toggleInstruction:basicInstructions) filename23
-     let compute23 a = fmap fst
-                     $ runMachineT initialRegisters
-                     $ runToggleT (length program23)
-                     $ A =: a >> runUntilEnd program23 >> reg A
-     putStrLn "Solutions for problem 23"
+     let compute23 a = evalMachineT $ runToggleT (length program23)
+                     $ do A =: a; runUntilEnd program23; reg A
      print =<< compute23  7
-     -- print =<< compute23 12
+     -- print =<< compute23 12 -- takes about 2 minutes on my computer
 
+     putStrLn "Problem 25"
      program25 <- loadFile (outputInstruction:basicInstructions) filename25
-     putStrLn "Solutions for problem 25"
      print $ find (isGoodLoop program25) [1..]
 
 
@@ -80,22 +77,23 @@ class Monad m => MonadRegisters m where
   reg  :: Register -> m Int
   (=:) :: Register -> Int -> m ()
 
-(-=), (+=) :: MonadRegisters m => Register -> Int -> m ()
-r -= d = r += (-d)
-r += d = do x <- reg r
-            r =: x+d
-
+(+=), (-=) :: MonadRegisters m => Register -> Int -> m ()
+r += d = do x <- reg r; r =: x+d
+r -= d = r += negate d
 infix 2 =:, +=, -=
 
+-- | Instructions common to problems 12, 23, and 25
 class Monad m => MonadMachine m where
-  inc :: Register -> m ()
-  dec :: Register -> m ()
+  inc :: Register          -> m ()
+  dec :: Register          -> m ()
   cpy :: Value -> Register -> m ()
-  jnz :: Value -> Value -> m ()
+  jnz :: Value -> Value    -> m ()
 
+-- | Instructions unique to problem 23
 class Monad m => MonadOutput m where
   out :: Value -> m ()
 
+-- | Instructions unique to problem 25
 class Monad m => MonadToggle m where
   tgl :: Value -> m ()
 
@@ -110,13 +108,16 @@ newtype MachineT m a = Machine (StateT Registers m a)
 runMachineT :: Monad m => Registers -> MachineT m a -> m (a, Registers)
 runMachineT r (Machine m) = runStateT m r
 
+evalMachineT :: Monad m => MachineT m a -> m a
+evalMachineT m = fst <$> runMachineT initialRegisters m
+
 instance PrimMonad m => PrimMonad (MachineT m) where
   type PrimState (MachineT m) = PrimState m
-  primitive = Machine . primitive
+  primitive = lift . primitive
 
 instance Monad m => MonadRegisters (MachineT m) where
-  reg r =
-    do Registers pc a b c d <- Machine get
+  reg r = Machine $
+    do Registers pc a b c d <- get
        return $! case r of PC -> pc; A -> a; B -> b; C -> c; D -> d
   r =: x = Machine $
        modify' $ \rs -> case r of
@@ -129,17 +130,13 @@ instance Monad m => MonadRegisters (MachineT m) where
   {-# INLINE reg #-}
 
 instance Monad m => MonadMachine (MachineT m) where
-  inc r   = r += 1             >> PC += 1
-  dec r   = r -= 1             >> PC += 1
-  cpy i o = value i >>= (o =:) >> PC += 1
+  inc r   = do r += 1;               PC += 1
+  dec r   = do r -= 1;               PC += 1
+  cpy i o = do v <- value i; o =: v; PC += 1
   jnz cond offset =
     do x <- value cond
        o <- if x == 0 then return 1 else value offset
        PC += o
-  {-# INLINE inc #-}
-  {-# INLINE dec #-}
-  {-# INLINE cpy #-}
-  {-# INLINE jnz #-}
 
 
 ------------------------------------------------------------------------
@@ -166,13 +163,15 @@ instance MonadMachine m => MonadMachine (OutputT m) where
 
 -- | Output values checking for correct alternation of zero and one
 instance (MonadRegisters m, MonadPlus m) => MonadOutput (OutputT m) where
-  out v = value v >>= outputPrim >> PC += 1
+  out o =
+    do x <- value o
+       n <- plusOne
+       guard (x == if even n then 0 else 1)
+       PC += 1
 
-outputPrim :: MonadPlus m => Int -> OutputT m ()
-outputPrim x = Output $
-  do n <- get
-     put (n+1)
-     guard (x == if even n then 0 else 1)
+-- | Increment the output counter returning the old value
+plusOne :: Monad m => OutputT m Int
+plusOne = Output $ state $ \n -> let n' = n+1 in n' `seq` (n,n')
 
 ------------------------------------------------------------------------
 -- ToggleT: monad transformer implementing MonadToggle
@@ -200,18 +199,12 @@ needReg _ _                 = PC += 1
 instance MonadRegisters m => MonadRegisters (ToggleT m) where
   x =: y = lift (x =: y)
   reg x  = lift (reg x)
-  {-# INLINE (=:) #-}
-  {-# INLINE reg #-}
 
 instance (PrimMonad m, MonadRegisters m, MonadMachine m) => MonadMachine (ToggleT m) where
   inc x   = toggler' (inc x)   (dec x)
   dec x   = toggler' (dec x)   (inc x)
   jnz x y = toggler' (jnz x y) (cpy x `needReg` y)
   cpy x y = toggler' (cpy x y) (jnz x (ValueRegister y))
-  {-# INLINE inc #-}
-  {-# INLINE dec #-}
-  {-# INLINE cpy #-}
-  {-# INLINE jnz #-}
 
 instance (PrimMonad m, MonadRegisters m, MonadMachine m) => MonadToggle (ToggleT m) where
   tgl x = toggler (tglPrim x) (inc `needReg` x)
@@ -241,13 +234,11 @@ toggler normal toggled =
 
 -- | Big-step machine semantics. Runs machine until it halts.
 runUntilEnd :: MonadRegisters m => Vector (m a) -> m ()
-runUntilEnd program = go
-  where
-    go =
-      do pc <- reg PC
-         case program Vector.!? pc of
-           Nothing -> return ()
-           Just m  -> m >> go
+runUntilEnd program = go where
+  go = do pc <- reg PC
+          case program Vector.!? pc of
+            Nothing -> return ()
+            Just m  -> do m; go
 
 
 -- | Predicate for machines that loop while producing a good output stream.
@@ -259,12 +250,12 @@ isGoodLoop program start = isWorking (go s0 <$> step s0)
 
     -- small-step machine semantics
     step (outs, regs) =
-      do op                 <- program Vector.!? registerPC regs
-         (((),outs'),regs') <- runMachineT regs $ runOutputT outs op
+      do op                <- program Vector.!? registerPC regs
+         ((_,outs'),regs') <- runMachineT regs (runOutputT outs op)
          return (outs', regs')
 
     -- machine produced a postive, even number of outputs
-    isProductive (out1, _) (out2, _) =  out1 /= out2 && even (out1 - out2)
+    isProductive (out1, _) (out2, _) = out1 /= out2 && even (out1 - out2)
 
     -- machine is at same PC with same registers
     isLooping (_, regs1) (_, regs2) = regs1 == regs2
@@ -285,35 +276,30 @@ isGoodLoop program start = isWorking (go s0 <$> step s0)
 -- | Parse the given file using the given set of instructions.
 loadFile :: [(String, Parser a)] -> FilePath -> IO (Vector a)
 loadFile parsers filename =
-  do txt <- readFile filename
+  do txt <- Text.readFile filename
      case parse (parseLines parsers) filename txt of
        Left e  -> throwIO e
        Right p -> return $! Vector.fromList p
 
 -- | Parse the whole input as lines of instructions.
 parseLines :: [(String, Parser a)] -> Parser [a]
-parseLines parsers = many (parseLine parsers) <* eof
+parseLines parsers = sepEndBy (dispatch parsers) eol <* eof
 
--- | Parse a single file line using the given parsers.
-parseLine :: [(String,Parser a)] -> Parser a
-parseLine xs =
-  do name <- pInstructionName
-     case lookup name xs of
-       Just p  -> p <* eol
-       Nothing -> fail "unknown instruction"
+-- | Select a parser by selecting via the next alphabetic lexeme
+dispatch :: [(String,Parser a)] -> Parser a
+dispatch xs =
+  do lexeme <- lexeme (some letterChar)
+     case lookup lexeme xs of
+       Just p  -> p
+       Nothing -> fail ("unexpected '" ++ lexeme ++ "'")
 
 -- | Wrap a parser to handle trailing whitespace
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme (skipMany (char ' '))
 
--- | Returns the next instruction name
-pInstructionName :: Parser String
-pInstructionName = lexeme (some letterChar)
-
 -- | Parse a single register
 pRegister :: Parser Register
-pRegister =
-  choice [ r <$ lexeme (char c) | (r,c) <- [(A,'a'),(B,'b'),(C,'c'),(D,'d')]]
+pRegister = dispatch [("a",pure A),("b",pure B),("c",pure C),("d",pure D)]
 
 -- | Parse a register or a integer constant
 pValue :: Parser Value
@@ -349,20 +335,20 @@ outputInstruction = ("out", liftA out pValue)
 newtype Render a = Render (Writer (Endo String) a)
   deriving (Functor, Applicative, Monad)
 
-execRender :: Render a -> String
-execRender (Render m) = appEndo (execWriter m) ""
+toShowS :: Render a -> String
+toShowS (Render m) = execWriter m `appEndo` ""
 
-renderS :: ShowS -> Render ()
-renderS f = Render (tell (Endo f))
+fromShowS :: ShowS -> Render ()
+fromShowS = Render . tell . Endo
 
 class Renderable a where
   render     :: a -> Render ()
   renderList :: [a] -> Render ()
-  renderList = mapM_ render
+  renderList = traverse_ render
 
 instance Renderable Char where
-  render     = renderS . showChar
-  renderList = renderS . showString
+  render     = fromShowS . showChar
+  renderList = fromShowS . showString
 
 instance Renderable a => Renderable [a] where
   render = renderList
@@ -372,10 +358,10 @@ instance Renderable Value where
   render (ValueInt      i) = render i
 
 instance Renderable Int where
-  render = renderS . shows
+  render = fromShowS . shows
 
 instance Renderable Register where
-  render r = render (case r of PC -> "pc"; A -> "a"; B -> "b"; C -> "c"; D -> "d")
+  render r = render $ case r of PC -> "pc"; A -> "a"; B -> "b"; C -> "c"; D -> "d"
 
 
 instance MonadMachine Render where
